644924f61358c6e848da645f71b7571d322b6546
simbernecky11@hotmail.com <>
2018-10-22 15:37:35 -0400

    Testing

---
+++ src/blah/Blah.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blah;
+
+/**
+ *
+ * @author simbe
+ */
+public class Blah {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+     Bernecky();
+    }
+    public static void Bernecky()
+    {
+        System.out.print("TESTING");
+    }
+}
